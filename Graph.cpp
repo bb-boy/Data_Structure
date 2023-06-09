6261fcf3ef4f1ef47f93b0709231df9b0692f2fd
jesus <jack520404@gmail.com>
2023-06-10 02:45:26 +0800

    增加图的广度和深度遍历

--- Graph.cpp
+++ Graph.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
 #define num(a) sizeof(a) / sizeof(a[0])
+#define MAX 100
 
 typedef struct Edge {                 //边节点
 	int pos;
@@ -40,7 +41,7 @@ void link_edge_node(VNode &v, edge_point e) {
 
 /*创建有向邻接表*/
 Graph Create_Graph1(Graph& gp, char edge[][2]) {
-	for (int i = 0; i < gp.vexnum; i++) {
+	for (int i = 0; i < gp.edgenum; i++) {
 		char c1 = edge[i][0];
 		char c2 = edge[i][1];
 		int a1 = pos(gp, c1);
@@ -65,7 +66,7 @@ Graph Create_Graph1(Graph& gp, char edge[][2]) {
 
 /*创建邻接表*/
 Graph Create_Graph(Graph &gp,char edge[][2]) {
-	for (int i = 0; i < gp.vexnum; i++) {
+	for (int i = 0; i < gp.edgenum; i++) {
 		char c1 = edge[i][0];
 		char c2 = edge[i][1];
 		int a1 = pos(gp, c1);
@@ -91,10 +92,10 @@ Graph Create_Graph(Graph &gp,char edge[][2]) {
 	return gp;
 }
 
-/*打印邻接表*/
+/*打印无向邻接表*/
 void print_Graph(Graph gp) {
 	for (int i = 0; i < gp.vexnum; i++) {
-		
+
 		edge_point p = gp.vexs[i].first_edge;
 		if (NULL == p)
 			continue;
@@ -106,16 +107,86 @@ void print_Graph(Graph gp) {
 	}
 }
 
+/*深度优先遍历*/
+
+
+void DFS(Graph gp,int i,int *visited) {
+	visited[i] = 1;
+	printf("%c", gp.vexs[i].data);
+	edge_point p = gp.vexs[i].first_edge;
+	while (p != NULL) {
+		int pos = p->pos;
+		if (!visited[pos]) {
+			visited[pos] = 1;
+			printf("%c", gp.vexs[pos].data);
+			p = gp.vexs[pos].first_edge;
+
+		}
+		else
+			p = p->next_edge;
+
+	}
+}
+	
+void DFSTraverse(Graph gp) {
+	int visited[MAX];
+	for (int i = 0; i < gp.vexnum; i++)
+		visited[i] = 0;
+	for (int i = 0; i < gp.vexnum; i++) {
+		if (!visited[i])
+			DFS(gp, i, visited);
+	}
+}
+	
+
+/*广度优先遍历*/
+void BFS(Graph gp) {
+	int queue[MAX];
+	int rear = 0,head = 0;
+	int visited[MAX];
+	for (int i = 0; i < gp.vexnum; i++)
+		visited[i] = 0;
+
+	for (int i = 0; i < gp.vexnum; i++) {
+		if (!visited[i]) {
+			visited[i] = 1;
+			printf("%c", gp.vexs[i].data);
+			queue[rear++] = i;
+			while (rear != head) {
+				int j = queue[head++];
+				edge_point node = gp.vexs[j].first_edge;
+				while (NULL != node) {
+					int pos = node->pos;
+					if (!visited[pos]) {
+						visited[pos] = 1;
+						printf("%c", gp.vexs[pos].data);
+						queue[rear++] = pos;
+
+
+					}
+					node = node->next_edge;
+				}
+			}
+
+			
+		}
+	}
+	
+	
+
+}
 
 int main() {
 	char vex[] = { 'A','B','C','D','E','F','G' };
 	char edge[][2] = {
 		{'A','B'},
-		{'A','C'},
-		{'A','D'},
+		{'B','E'},
 		{'B','C'},
-		{'C','D'},
-		{'E','G'},
+		{'B','F'},
+		{'C','E'},
+		{'D','C'},
+		{'E','B'},
+		{'E','D'},
 		{'F','G'}
 	};
 
@@ -137,12 +208,15 @@ int main() {
 	}
 
 	/*创建有向邻接表*/
-	Create_Graph(gp, edge);
-	print_Graph(gp);
+	//Create_Graph(gp, edge);
+	//print_Graph(gp);
 
 	/*创建有向邻接表*/
 	Create_Graph1(gp, edge);
 	print_Graph(gp);
 
+	//DFSTraverse(gp);
+	BFS(gp);
 
+	
 }
